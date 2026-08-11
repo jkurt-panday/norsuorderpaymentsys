0a0a3e68fea226e7f233c472f1f8ac847a927b0f
kurt_ <pandaykurt427@gmail.com>
2026-08-11 14:01:36 +0800

    added bankaccounts seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use App\Models\Membership;
 use App\Models\PaymentDetailOption;
+use App\Models\BankAccountInfo;
+use App\Models\Uacs;
 
 class DatabaseSeeder extends Seeder
 {
@@ -42,28 +44,28 @@ class DatabaseSeeder extends Seeder
         $this->command->warn('Seeded credentials must be rotated before production use.');
 
         $memberships = [
-                   [
-                       'member_code' => 'NAPU',
-                       'member_desc' => 'NORSU Administrative Personnel Union',
-                   ],
-                   [
-                       'member_code' => 'NORSUFFA',
-                       'member_desc' => 'Negros Oriental State University Federated Faculty Association',
-                   ],
-                   [
-                       'member_code' => 'N/A',
-                       'member_desc' => 'Not Applicable',
-                   ],
-               ];
-       
-               foreach ($memberships as $membership) {
-                   Membership::updateOrCreate(
-                       ['member_code' => $membership['member_code']],
-                       ['member_desc' => $membership['member_desc']]
-                   );
-               }
-       
-               $this->command->info('Successfully seeded: Memberships');
+            [
+                'member_code' => 'NAPU',
+                'member_desc' => 'NORSU Administrative Personnel Union',
+            ],
+            [
+                'member_code' => 'NORSUFFA',
+                'member_desc' => 'Negros Oriental State University Federated Faculty Association',
+            ],
+            [
+                'member_code' => 'N/A',
+                'member_desc' => 'Not Applicable',
+            ],
+        ];
+
+        foreach ($memberships as $membership) {
+            Membership::updateOrCreate(
+                ['member_code' => $membership['member_code']],
+                ['member_desc' => $membership['member_desc']]
+            );
+        }
+
+        $this->command->info('Successfully seeded: Memberships');
        
                /*
                |--------------------------------------------------------------------------
@@ -71,31 +73,92 @@ class DatabaseSeeder extends Seeder
                |--------------------------------------------------------------------------
                */
        
-               $paymentOptions = [
-                   [
-                       'payment_desc' => 'Comprehensive Exam - Doctorate',
-                   ],
-                   [
-                       'payment_desc' => 'Comprehensive Exam - Masters',
-                   ],
-                   [
-                       'payment_desc' => 'IGP FABLAB',
-                   ],
-               ];
+        $paymentOptions = [
+            [
+                'payment_desc' => 'Comprehensive Exam - Doctorate',
+            ],
+            [
+                'payment_desc' => 'Comprehensive Exam - Masters',
+            ],
+            [
+                'payment_desc' => 'IGP FABLAB',
+            ],
+        ];
        
-               foreach ($paymentOptions as $option) {
-                   PaymentDetailOption::updateOrCreate(
-                       ['payment_desc' => $option['payment_desc']],
-                       $option
-                   );
-               }
+        foreach ($paymentOptions as $option) {
+            PaymentDetailOption::updateOrCreate(
+                ['payment_desc' => $option['payment_desc']],
+                $option
+            );
+        }
+
+        $this->command->info(
+            'Successfully seeded: Payment Detail Options'
+        );
+
+        $this->command->warn(
+            'Seeded credentials must be rotated before production use.'
+        );
+
+        // $table->string('account_name', 255);
+        // $table->string('fund_cluster', 50);
+        // $table->string('bank_name', 100);
+        // $table->string('account_num', 50)->unique();
+
+        $bankaccount = [
+            [
+                'account_name' => 'NORSU Special Trust Fund 164 - Miscellaneous',
+                'fund_cluster' => 'M-164',
+                'bank_name' => 'DBP Dumaguete',
+                'account_num' => '0740-012653-080',
+            ],
+            [
+                'account_name' => 'NORSU Special Trust Fund 164 - Tuition',
+                'fund_cluster' => 'T-164',
+                'bank_name' => 'DBP Dumaguete',
+                'account_num' => '0740-012637-032',
+            ],
+        ];
        
-               $this->command->info(
-                   'Successfully seeded: Payment Detail Options'
-               );
+        foreach ($bankaccount as $option) {
+            BankAccountInfo::updateOrCreate(
+                ['account_name' => $option['account_name']],
+                ['func_cluster' => $option['func_cluster']],
+                ['bank_name' => $option['bank_name']],
+                ['account_num' => $option['account_num']],
+            );
+        }
+
+        $this->command->info(
+            'Successfully seeded: Bank Account Info'
+        );
+
+        // $uacs = [
+        //     [
+        //         'account_name' => 'NORSU Special Trust Fund 164 - Miscellaneous',
+        //         'fund_cluster' => 'M-164',
+        //         'bank_name' => 'DBP Dumaguete',
+        //         'account_num' => '0740-012653-080',
+        //     ],
+        //     [
+        //         'account_name' => 'NORSU Special Trust Fund 164 - Tuition',
+        //         'fund_cluster' => 'T-164',
+        //         'bank_name' => 'DBP Dumaguete',
+        //         'account_num' => '0740-012637-032',
+        //     ],
+        // ];
        
-               $this->command->warn(
-                   'Seeded credentials must be rotated before production use.'
-               );
+        // foreach ($bankaccount as $option) {
+        //     BankAccountInfo::updateOrCreate(
+        //         ['account_name' => $option['account_name']],
+        //         ['func_cluster' => $option['func_cluster']],
+        //         ['bank_name' => $option['bank_name']],
+        //         ['account_num' => $option['account_num']],
+        //     );
+        // }
+
+        // $this->command->info(
+        //     'Successfully seeded: Bank Account Info'
+        // );
     }
 }
