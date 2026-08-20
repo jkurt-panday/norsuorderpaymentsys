653502593f3cbbd95fa3da49ca84104f9dd3c2f8
kurt_ <pandaykurt427@gmail.com>
2026-08-20 09:19:17 +0800

    actions button

--- resources/js/pages/staff/courses/columns.tsx
+++ resources/js/pages/staff/courses/columns.tsx
@@ -1,7 +1,8 @@
 import type { ColumnDef, TableFeatures } from '@tanstack/react-table';
-import { MoreHorizontal } from 'lucide-react';
+import { Pencil, Trash2 } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
+import { Link } from '@inertiajs/react';
 
 export type Course = {
     id: number;
@@ -12,9 +13,14 @@ export type Course = {
 };
 
 export const columns: ColumnDef<TableFeatures, Course>[] = [
+    // {
+    //     accessorKey: 'id',
+    //     header: 'ID',
+    // },
     {
-        accessorKey: 'id',
+        id: 'row_number',
         header: 'ID',
+        cell: ({ row }) => row.index + 1,
     },
 
     {
@@ -34,13 +40,21 @@ export const columns: ColumnDef<TableFeatures, Course>[] = [
             const course = row.original;
 
             return (
-                <Button
-                    variant="ghost"
-                    size="icon"
+                <div className="grid w-max grid-cols-2">
+                    <Link
+                          href={`/staff/courses/${course.id}/edit`}
+                    className="flex h-8 w-8 items-center justify-center rounded-l-2xl bg-amber-400 text-white transition-colors hover:bg-amber-500"
+                  >
+                    <Pencil className="h-4 w-4" />
+                  </Link>
+                
+                  <Link
+                    className="flex h-8 w-8 items-center justify-center rounded-r-2xl border-white/20 bg-red-600 text-white transition-colors hover:bg-red-700"
                     onClick={() => console.log(course)}
-                >
-                    <MoreHorizontal />
-                </Button>
+                  >
+                    <Trash2 className="h-4 w-4" />
+                  </Link>
+                </div>
             );
         },
     },
