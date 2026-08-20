b1efbe817fc84728b7e64db712fd434affc6b99a
TroyNPC <grefaldetroyg@gmail.com>
2026-08-20 17:53:42 +0800

    Changed admin/staff routes to .staffdashboard (to be changed inside app.php)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -40,8 +40,8 @@ return Application::configure(basePath: dirname(__DIR__))
                 }
 
                 $targetRoute = match ($user->role) {
-                    'admin' => 'admin.users.index',
-                    'staff' => 'staff.dashboard',
+                    'admin' => 'admin.staffdashboard',
+                    'staff' => 'staff.staffdashboard',
                     default => 'client.dashboard',
                 };
 
