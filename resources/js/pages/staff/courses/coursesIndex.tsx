741b5aa67f633bfcb8918bd82de4c38bc4b5ab80
kurt_ <pandaykurt427@gmail.com>
2026-08-25 10:29:51 +0800

    removed unused components

--- resources/js/pages/staff/courses/coursesIndex.tsx
+++ resources/js/pages/staff/courses/coursesIndex.tsx
@@ -3,9 +3,6 @@ import {
     Card,
     CardHeader,
     CardTitle,
-    CardDescription,
-    CardContent,
-    CardFooter,
 } from '@/components/ui/card';
 import { Button } from '@/components/base/buttons/button';
 import { Plus } from 'lucide-react';
