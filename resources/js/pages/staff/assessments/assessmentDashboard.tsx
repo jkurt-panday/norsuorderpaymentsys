a23d526e26daf7694776d3dfd9bc51a822ca6f2e
kurt_ <pandaykurt427@gmail.com>
2026-08-27 09:28:19 +0800

    added header for assessment dashboard

--- resources/js/pages/staff/assessments/assessmentDashboard.tsx
+++ resources/js/pages/staff/assessments/assessmentDashboard.tsx
@@ -1,7 +1,19 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Head } from '@inertiajs/react';
+
 export default function AssessmentDashboard() {
     return (
         <>
-            this is the dashboard page where the statistics chart are rendered
+            <div className="mx-auto min-h-screen w-full max-w-7xl min-w-0 space-y-4 bg-slate-50 p-3 sm:p-6">
+                <Head title="Assessment Dashboard" />
+                <Card className="rounded-2xl border border-slate-200/70 bg-white px-2! py-2! shadow-sm">
+                    <CardHeader className="flex flex-row items-center justify-between p-3">
+                        <CardTitle className="text-xl font-bold text-slate-900">
+                            Assessments Dashboard
+                        </CardTitle>
+                    </CardHeader>
+                </Card>
+            </div>
         </>
-  );
-};
+    );
+}
