f7bb883af7ebd360ad4306c5d4461c9828566518
kurt_ <pandaykurt427@gmail.com>
2026-08-28 13:04:48 +0800

    added card header

---
+++ resources/js/pages/staff/assessments/assessmentEdit.tsx
@@ -0,0 +1,19 @@
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Head } from '@inertiajs/react';
+
+export default function AssessmentEdit() {
+    return (
+        <>
+        <div className="mx-auto min-h-screen w-full max-w-7xl min-w-0 space-y-4 bg-slate-50 p-3 sm:p-6">
+            <Head title="Assessment Index" />
+            <Card className="rounded-2xl border border-slate-200/70 bg-white px-2! py-2! shadow-sm">
+                <CardHeader className="flex flex-row items-center justify-between p-3">
+                    <CardTitle className="text-xl font-bold text-slate-900">
+                        Assessments
+                    </CardTitle>
+                </CardHeader>
+                </Card>
+        </div>
+        </>
+    );
+}
