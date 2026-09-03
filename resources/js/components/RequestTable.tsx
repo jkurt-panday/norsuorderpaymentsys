716c74becb67a02ec62a64c3494f22e1628f24f6
kurt_ <pandaykurt427@gmail.com>
2026-09-03 13:04:22 +0800

    removed <<<<<

--- resources/js/components/RequestTable.tsx
+++ resources/js/components/RequestTable.tsx
@@ -802,19 +802,7 @@ export default function RequestTable<T extends { id: number | string }>({
                                                 }
                                             }}
                                         >
-<<<<<<< HEAD
-                                            <PopoverTrigger
-                                                render={
-                                                    <button
-                                                        type="button"
-                                                        className="inline-flex h-8 items-center justify-center gap-1 rounded-md border border-slate-200 px-3 text-sm font-medium text-slate-600 transition-colors hover:bg-slate-50"
-                                                    />
-                                                }
-                                            >
-                                                Page {resource.current_page} of{' '}
-                                                {resource.last_page}
-=======
-                                            <PopoverTrigger asChild>
+                                            <PopoverTrigger>
                                                 <span
                                                     role="button"
                                                     tabIndex={0}
@@ -823,7 +811,6 @@ export default function RequestTable<T extends { id: number | string }>({
                                                     Page {resource.current_page}{' '}
                                                     of {resource.last_page}
                                                 </span>
->>>>>>> f5701c87f22c935eb0964ba535e3805bd26f36be
                                             </PopoverTrigger>
                                             <PopoverContent
                                                 align="center"
